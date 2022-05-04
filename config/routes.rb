484ffe4893fb48ea66a0ca7b169fee775d10db8b
Adam Heckathorn <ajheckathorn01@gmail.com>
2022-05-04 17:54:44 -0500

    shortcut routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/all_products" => "products#all_products"
+  get "/first_product" => "products#first_product"
 end
